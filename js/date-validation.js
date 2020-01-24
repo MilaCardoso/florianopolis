4d6874710937afba496b10a3f591eabb9bc13242
MilaCardoso <camilalacardoso@gmail.com>
2020-01-24 19:58:40 +0000

    Date script fix

--- js/date-validation.js
+++ js/date-validation.js
@@ -1,9 +1,10 @@
 function TDate(element) {
     var UserDate = element.value;
     var ToDate = new Date();
-    if (new Date(UserDate).getTime() <= ToDate.getTime()) {
+    ToDate.setHours(0,0,0,0);
+    if (new Date(UserDate).getTime() < ToDate.getTime()) {
           alert("The Date must be Bigger or Equal to today Date");
-          element.value = "";
+          element.value ="";
           return false;
      }
     return true;
