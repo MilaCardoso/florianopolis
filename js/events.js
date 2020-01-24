2047041012fc591ce2bec8e8eaac53b038a1a15e
MilaCardoso <camilalacardoso@gmail.com>
2020-01-24 23:15:52 +0000

    Event page

---
+++ js/events.js
@@ -0,0 +1,6 @@
+function getEvents() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "data/events.txt", false);
+    xmlhttp.send();
+    document.getElementById("events").innerHTML = xmlhttp.responseText;
+}
